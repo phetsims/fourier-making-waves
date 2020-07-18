1e518e9f8f5c138b1fb06f8e3cd955dc00491a5a
pixelzoom <cmalley@pixelzoom.com>
2020-07-18 13:00:54 -0600

    TODO

--- js/fourier-making-waves-main.js
+++ js/fourier-making-waves-main.js
@@ -18,7 +18,7 @@ const fourierMakingWavesTitleString = fourierMakingWavesStrings[ 'fourier-making
 
 const simOptions = {
   credits: {
-    //TODO fill in credits, all of these fields are optional, see joist.CreditsNode
+    //TODO https://github.com/phetsims/fourier-making-waves/issues/2 complete credits
     leadDesign: 'Amy Rouinfar',
     softwareDevelopment: 'Chris Malley (PixelZoom, Inc.)',
     team: 'Wendy Adams, Mike Dubson, Danielle Harlow, Sam McKagan, Ariel Paul, Kathy Perkins, Carl Weiman',
