08fb9414cc6ac80242e5b1deb156517960fbeea6
samreid <reids@colorado.edu>
2020-11-10 08:40:19 -0700

    Use XYChartNode to render the amplitude chart background, see https://github.com/phetsims/fourier-making-waves/issues/8

--- js/discrete/view/DiscreteAmplitudesPanel.js
+++ js/discrete/view/DiscreteAmplitudesPanel.js
@@ -9,6 +9,7 @@
 
 import NumberProperty from '../../../../axon/js/NumberProperty.js';
 import Range from '../../../../dot/js/Range.js';
+import XYChartNode from '../../../../griddle/js/XYChartNode.js';
 import merge from '../../../../phet-core/js/merge.js';
 import HBox from '../../../../scenery/js/nodes/HBox.js';
 import HStrut from '../../../../scenery/js/nodes/HStrut.js';
@@ -20,6 +21,11 @@ import AmplitudeSlider from '../../common/view/AmplitudeSlider.js';
 import FourierMakingWavesPanel from '../../common/view/FourierMakingWavesPanel.js';
 import fourierMakingWaves from '../../fourierMakingWaves.js';
 import fourierMakingWavesStrings from '../../fourierMakingWavesStrings.js';
+
+// constants
+
+// Font size for the numeric tick marks (on the left) and the "n" label on the right
+const LABEL_FONT_SIZE = 15;
 
 class DiscreteAmplitudesPanel extends FourierMakingWavesPanel {
 
@@ -38,20 +44,73 @@ class DiscreteAmplitudesPanel extends FourierMakingWavesPanel {
       font: FourierMakingWavesConstants.TITLE_FONT
     } );
 
-    //TODO a test of AmplitudeSlider
     const amplitudeRange = new Range( -1.27, 1.27 );
-    const sliders = _.map( FourierMakingWavesColors.HARMONIC_COLORS, color =>
-      new AmplitudeSlider( new NumberProperty( 0, { range: amplitudeRange } ), { color: color } )
-    );
-    const slidersLayoutBox = new HBox( {
-      spacing: 10,
-      children: [ new HStrut( 120 ), ...sliders ]
+
+    // The XYChartNode is just used to render the chart area, not for rendering any data sets.  The AmplitudeSlider
+    // instances will be added to it as children.
+    const xyChartNode = new XYChartNode( {
+
+      // Dimensions
+      width: 610,
+      height: 120,
+
+      // Use the same range as the sliders.  Note the chart extends beyond -1..1, like a margin.
+      defaultModelYRange: amplitudeRange,
+
+      // Border for the chart.
+      chartPanelOptions: {
+        lineWidth: 2
+      },
+
+      showHorizontalGridLabels: false,
+      verticalGridLabelNumberOfDecimalPlaces: 1,
+      majorHorizontalLineSpacing: 0.5,
+      gridNodeOptions: {
+
+        // Don't show vertical lines within the chart
+        majorVerticalLineSpacing: null
+      },
+      gridLabelOptions: {
+        fontSize: LABEL_FONT_SIZE
+      }
     } );
+
+    // Compute the track height based on the dimensions in the chart, so it will match up
+    const trackHeight = Math.abs( xyChartNode.modelViewTransformProperty.value.modelToViewDeltaY( amplitudeRange.getLength() ) );
+
+    //TODO a test of AmplitudeSlider
+    const sliders = _.map( FourierMakingWavesColors.HARMONIC_COLORS, color =>
+      new AmplitudeSlider( new NumberProperty( 0, { range: amplitudeRange } ), {
+        color: color,
+        trackHeight: trackHeight
+      } )
+    );
+
+    // Solve for spacing between sliders
+    // n * sliderWidth + (n-1) * spacing +margin*2 = totalWidth
+    // n * sliderWidth + n*spacing - spacing + margin * 2 = totalWidth
+    // spacing ( n - 1) + n*sliderWidth + margin*2 = totalWidth
+    const margin = 10;
+    const spacing = ( xyChartNode.chartPanel.width - sliders.length * sliders[ 0 ].width - margin * 2 ) / ( sliders.length - 1 );
+    const slidersLayoutBox = new HBox( {
+      spacing: spacing,
+      children: sliders,
+      x: margin,
+      centerY: xyChartNode.chartHeight / 2
+    } );
+
+    // Put the sliders in the xyChartNode instead of the xyChartNode.chartPanel so they won't be clipped
+    xyChartNode.addChild( slidersLayoutBox );
 
     const content = new VBox( {
       align: 'left',
       spacing: 5,
-      children: [ titleNode, slidersLayoutBox ]
+      children: [ titleNode, new HBox( {
+        spacing: 5,
+        children: [ new HStrut( 75 ), xyChartNode, new Text( 'n', {
+          fontSize: 14
+        } ) ]
+      } ) ]
     } );
 
     super( content, options );
