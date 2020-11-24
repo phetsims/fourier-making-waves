7d7e512aea30635844458d6d740b406f2f14c01d
pixelzoom <cmalley@pixelzoom.com>
2020-11-24 15:00:36 -0700

    map amplitude to output level

--- js/discrete/view/FourierSoundGenerator.js
+++ js/discrete/view/FourierSoundGenerator.js
@@ -6,28 +6,41 @@
  * @author Chris Malley (PixelZoom, Inc.)
  */
 
+import NumberProperty from '../../../../axon/js/NumberProperty.js';
+import LinearFunction from '../../../../dot/js/LinearFunction.js';
+import Range from '../../../../dot/js/Range.js';
 import AssertUtils from '../../../../phetcommon/js/AssertUtils.js';
 import OscillatorSoundGenerator from '../../../../tambo/js/sound-generators/OscillatorSoundGenerator.js';
 import SoundGenerator from '../../../../tambo/js/sound-generators/SoundGenerator.js';
 import FourierSeries from '../../common/model/FourierSeries.js';
 import fourierMakingWaves from '../../fourierMakingWaves.js';
 
+// constants
+const OUTPUT_LEVEL_RANGE = new Range( -1, 1 );
+
 class FourierSoundGenerator extends SoundGenerator {
 
   /**
    * @param {FourierSeries} fourierSeries
    * @param {Property.<boolean>} enabledProperty
-   * @param {Property.<number>} outputLevelProperty
+   * @param {NumberProperty} outputLevelProperty
    */
   constructor( fourierSeries, enabledProperty, outputLevelProperty ) {
 
     assert && assert( fourierSeries instanceof FourierSeries, 'invalid fourierSeries' );
     assert && AssertUtils.assertPropertyOf( enabledProperty, 'boolean' );
-    assert && AssertUtils.assertPropertyOf( outputLevelProperty, 'number' );
+    assert && assert( outputLevelProperty instanceof NumberProperty, 'invalid outputLevelProperty' );
+    assert && assert( outputLevelProperty.range, 'outputLevelProperty.range required' );
 
     super();
 
     const maxNumberOfHarmonics = fourierSeries.numberOfHarmonicsProperty.range.max;
+
+    // Maps amplitude to an output level that is appropriate for SoundGenerator.
+    const amplitudeToOutputLevel = new LinearFunction(
+      fourierSeries.amplitudeRange.min, fourierSeries.amplitudeRange.max,
+      OUTPUT_LEVEL_RANGE.min, OUTPUT_LEVEL_RANGE.max
+    );
 
     // @private {OscillatorSoundGenerator[]}
     this.oscillatorSoundGenerators = [];
@@ -43,9 +56,9 @@ class FourierSoundGenerator extends SoundGenerator {
     // Set amplitudes for harmonics. unlink is not needed.
     fourierSeries.amplitudesProperty.link( amplitudes => {
 
-      // Set amplitudes for the relevant harmonics
+      // Set amplitudes for the relevant harmonics.
       for ( let i = 0; i < amplitudes.length; i++ ) {
-        this.oscillatorSoundGenerators[ i ].outputLevel = amplitudes[ i ];
+        this.oscillatorSoundGenerators[ i ].outputLevel = amplitudeToOutputLevel( amplitudes[ i ] );
       }
 
       // Set amplitudes of irrelevant harmonics to zero.
@@ -54,13 +67,13 @@ class FourierSoundGenerator extends SoundGenerator {
       }
     } );
 
-    // Set the master output level. unlink is not needed
+    // Set the master output level. unlink is not needed.
     outputLevelProperty.link( outputLevel => {
       this.outputLevel = outputLevel;
     } );
 
     //TODO can this be controlled via options.enableControlProperties?
-    // Turn sound on/off. unlink is not needed
+    // Turn sound on/off. unlink is not needed.
     enabledProperty.link( enabled => enabled ? this.play() : this.stop() );
   }
 
