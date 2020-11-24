9bb40ebc3dd879503be09df12ad295b20acde400
pixelzoom <cmalley@pixelzoom.com>
2020-11-24 15:29:06 -0700

    Disable sounds for UI components.

--- js/fourier-making-waves-main.js
+++ js/fourier-making-waves-main.js
@@ -8,6 +8,7 @@
 
 import Sim from '../../joist/js/Sim.js';
 import simLauncher from '../../joist/js/simLauncher.js';
+import soundManager from '../../tambo/js/soundManager.js';
 import Tandem from '../../tandem/js/Tandem.js';
 import ContinuousScreen from './continuous/ContinuousScreen.js';
 import DiscreteScreen from './discrete/DiscreteScreen.js';
@@ -30,13 +31,16 @@ const simOptions = {
   }
 };
 
-// launch the sim - beware that scenery Image nodes created outside of simLauncher.launch() will have zero bounds
-// until the images are fully loaded, see https://github.com/phetsims/coulombs-law/issues/70
 simLauncher.launch( () => {
+
   const sim = new Sim( fourierMakingWavesTitleString, [
     new DiscreteScreen( Tandem.ROOT.createTandem( 'discreteScreen' ) ),
     new WaveGameScreen( Tandem.ROOT.createTandem( 'waveGameScreen' ) ),
     new ContinuousScreen( Tandem.ROOT.createTandem( 'continuousScreen' ) )
   ], simOptions );
   sim.start();
+
+  //TODO do we want to enable user-interface sounds in 1.0 ?
+  // Disable sounds for UI components.
+  soundManager.setOutputLevelForCategory( 'user-interface', 0 );
 } );
