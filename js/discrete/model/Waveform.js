8bd0248a9f1b3f0e43a4c0dd2250e5e0b040655e
pixelzoom <cmalley@pixelzoom.com>
2021-02-22 14:20:59 -0700

    revert to Java equation for wave packet, https://github.com/phetsims/fourier-making-waves/issues/18

--- js/discrete/model/Waveform.js
+++ js/discrete/model/Waveform.js
@@ -98,36 +98,38 @@ const SAWTOOTH = new WaveformValue( ( numberOfHarmonics, seriesType ) => {
 //TODO https://github.com/phetsims/fourier-making-waves/issues/18 provide a reference for how amplitudes are computed
 const WAVE_PACKET = new WaveformValue( ( numberOfHarmonics, seriesType ) => {
 
-  // An = (1 / (p * sqrt(2 * PI))) ^ ((-(n-no))^2 / (2 * p^2))
+  //TODO https://github.com/phetsims/fourier-making-waves/issues/18 see p 26 of 'Fourier Design Outline'
+  // An = (1 / (p * sqrt(2 * PI)))^( -(n-no)^2 / (2 * p^2))
   // where p = 1.5, N = number of harmonics, no = (N + 1) / 2
 
   //TODO https://github.com/phetsims/fourier-making-waves/issues/18 this fails with invalid value, doesn't match hardcoded values from Preset.java
-  const amplitudes = [];
-  const p = 1.5;
-  const no = ( numberOfHarmonics + 1 ) / 2;
-  const base = 1 / ( p * Math.sqrt( 2 * PI ) );
-  for ( let n = 1; n <= numberOfHarmonics; n++ ) {
-    const exponent = Math.pow( -( n - no ), 2 ) / ( 2 * p * p );
-    const amplitude = Math.pow( base, exponent );
-    amplitudes.push( amplitude );
-  }
-  phet.log && phet.log( `wave packet: N=${numberOfHarmonics}, amplitudes=[${amplitudes}]` );
-  return amplitudes;
+  // const amplitudes = [];
+  // const p = 1.5;
+  // const no = ( numberOfHarmonics + 1 ) / 2;
+  // const base = 1 / ( p * Math.sqrt( 2 * PI ) );
+  // for ( let n = 1; n <= numberOfHarmonics; n++ ) {
+  //   const exponent = -Math.pow( n - no, 2 ) / ( 2 * p * p );
+  //   const amplitude = Math.pow( base, exponent );
+  //   amplitudes.push( amplitude );
+  // }
+  // console.log( `N=${numberOfHarmonics} amplitudes=${amplitudes}` );//TODO delete me
+  // debugger;
+  // return amplitudes;
 
   //TODO https://github.com/phetsims/fourier-making-waves/issues/18 workaround by using hardcoded values from Preset.java
-  // return [
-  //   [ 1.000000 ],
-  //   [ 0.457833, 0.457833 ],
-  //   [ 0.249352, 1.000000, 0.249352 ],
-  //   [ 0.172422, 0.822578, 0.822578, 0.172422 ],
-  //   [ 0.135335, 0.606531, 1.000000, 0.606531, 0.135335 ],
-  //   [ 0.114162, 0.457833, 0.916855, 0.916855, 0.457833, 0.114162 ],
-  //   [ 0.100669, 0.360448, 0.774837, 1.000000, 0.774837, 0.360448, 0.100669 ],
-  //   [ 0.091394, 0.295023, 0.644389, 0.952345, 0.952345, 0.644389, 0.295023, 0.091394 ],
-  //   [ 0.084658, 0.249352, 0.539408, 0.856997, 1.000000, 0.856997, 0.539408, 0.249352, 0.084658 ],
-  //   [ 0.079560, 0.216255, 0.457833, 0.754840, 0.969233, 0.969233, 0.754840, 0.457833, 0.216255, 0.079560 ],
-  //   [ 0.075574, 0.191495, 0.394652, 0.661515, 0.901851, 1.000000, 0.901851, 0.661515, 0.394652, 0.191495, 0.075574 ]
-  // ][ numberOfHarmonics - 1 ];
+  return [
+    [ 1.000000 ],
+    [ 0.457833, 0.457833 ],
+    [ 0.249352, 1.000000, 0.249352 ],
+    [ 0.172422, 0.822578, 0.822578, 0.172422 ],
+    [ 0.135335, 0.606531, 1.000000, 0.606531, 0.135335 ],
+    [ 0.114162, 0.457833, 0.916855, 0.916855, 0.457833, 0.114162 ],
+    [ 0.100669, 0.360448, 0.774837, 1.000000, 0.774837, 0.360448, 0.100669 ],
+    [ 0.091394, 0.295023, 0.644389, 0.952345, 0.952345, 0.644389, 0.295023, 0.091394 ],
+    [ 0.084658, 0.249352, 0.539408, 0.856997, 1.000000, 0.856997, 0.539408, 0.249352, 0.084658 ],
+    [ 0.079560, 0.216255, 0.457833, 0.754840, 0.969233, 0.969233, 0.754840, 0.457833, 0.216255, 0.079560 ],
+    [ 0.075574, 0.191495, 0.394652, 0.661515, 0.901851, 1.000000, 0.901851, 0.661515, 0.394652, 0.191495, 0.075574 ]
+  ][ numberOfHarmonics - 1 ];
 } );
 
 const CUSTOM = new WaveformValue( ( numberOfHarmonics, seriesType ) => {
