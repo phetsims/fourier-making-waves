975723b8c92dcbb5bb6cde6e9d34eff1914a9fff
pixelzoom <cmalley@pixelzoom.com>
2021-06-02 17:42:06 -0600

    start fleshing out chart parts

--- js/continuous/view/ContinuousAmplitudesChartNode.js
+++ js/continuous/view/ContinuousAmplitudesChartNode.js
@@ -8,10 +8,15 @@
 
 import ChartRectangle from '../../../../bamboo/js/ChartRectangle.js';
 import ChartTransform from '../../../../bamboo/js/ChartTransform.js';
+import LabelSet from '../../../../bamboo/js/LabelSet.js';
+import TickMarkSet from '../../../../bamboo/js/TickMarkSet.js';
 import Range from '../../../../dot/js/Range.js';
+import Utils from '../../../../dot/js/Utils.js';
 import merge from '../../../../phet-core/js/merge.js';
+import Orientation from '../../../../phet-core/js/Orientation.js';
 import AssertUtils from '../../../../phetcommon/js/AssertUtils.js';
 import StringUtils from '../../../../phetcommon/js/util/StringUtils.js';
+import Line from '../../../../scenery/js/nodes/Line.js';
 import Node from '../../../../scenery/js/nodes/Node.js';
 import RichText from '../../../../scenery/js/nodes/RichText.js';
 import Tandem from '../../../../tandem/js/Tandem.js';
@@ -22,6 +27,13 @@ import Domain from '../../common/model/Domain.js';
 import fourierMakingWaves from '../../fourierMakingWaves.js';
 import fourierMakingWavesStrings from '../../fourierMakingWavesStrings.js';
 import ContinuousWaveformCheckbox from './ContinuousWaveformCheckbox.js';
+
+// constants
+//TODO duplicated in WaveformChartNode
+const TICK_MARK_OPTIONS = {
+  edge: 'min',
+  extent: 6
+};
 
 //TODO placeholder
 class ContinuousAmplitudesChartNode extends Node {
@@ -59,27 +71,62 @@ class ContinuousAmplitudesChartNode extends Node {
       tandem: options.tandem.createTandem( 'chartRectangle' )
     } );
 
+    // x axis ---------------------------------------------------------
+
+    const xAxis = new Line( 0, 0, options.transformOptions.viewWidth, 0, {
+      stroke: FMWColorProfile.axisStrokeProperty,
+      lineWidth: 1,
+      centerX: chartRectangle.centerX,
+      centerY: chartRectangle.bottom
+    } );
+    const xTickMarks = new TickMarkSet( chartTransform, Orientation.HORIZONTAL, Math.PI / 2, TICK_MARK_OPTIONS );
+    const xTickLabels = new LabelSet( chartTransform, Orientation.HORIZONTAL, 2 * Math.PI, {
+      createLabel: createXTickLabel,
+      edge: 'min'
+    } );
     const xAxisLabel = new RichText( '', {
       font: FMWConstants.AXIS_LABEL_FONT,
       maxWidth: 60, // determined empirically
       tandem: options.tandem.createTandem( 'xAxisLabel' )
     } );
 
+    // y axis ---------------------------------------------------------
+
+    const yAxis = new Line( 0, 0, 0, options.transformOptions.viewHeight, {
+      stroke: FMWColorProfile.axisStrokeProperty,
+      lineWidth: 1,
+      centerX: chartRectangle.left,
+      centerY: chartRectangle.centerY
+    } );
+    const yAxisLabel = new RichText( fourierMakingWavesStrings.amplitude, {
+      font: FMWConstants.AXIS_LABEL_FONT,
+      rotation: -Math.PI / 2,
+      right: chartRectangle.left - FMWConstants.Y_AXIS_LABEL_SPACING,
+      centerY: chartRectangle.centerY,
+      maxWidth: 0.85 * chartRectangle.height,
+      tandem: options.tandem.createTandem( 'yAxisLabel' )
+    } );
+
+    // Addition UI components ---------------------------------------------------------
+
     const continuousWaveformCheckbox = new ContinuousWaveformCheckbox( continuousWaveformVisibleProperty, {
       right: chartRectangle.right - 5,
-      top: chartRectangle.bottom + 5,
+      top: xTickLabels.bottom + 7,
       tandem: options.tandem.createTandem( 'continuousWaveformCheckbox' )
     } );
 
     assert && assert( !options.children );
     options.children = [
+      xTickMarks, // ticks behind chartRectangle, so we don't see how they extend into chart's interior
       chartRectangle,
-      xAxisLabel,
+      xAxis, xAxisLabel, xTickLabels,
+      yAxis, yAxisLabel,
       continuousWaveformCheckbox
     ];
 
     super( options );
 
+    // Adjust the x-axis label to match the domain.
     domainProperty.link( domain => {
       xAxisLabel.text = StringUtils.fillIn( fourierMakingWavesStrings.xAxisLabel, {
         symbol: ( domain === Domain.SPACE ) ? FMWSymbols.k : FMWSymbols.omega,
@@ -91,6 +138,20 @@ class ContinuousAmplitudesChartNode extends Node {
       xAxisLabel.bottom = chartRectangle.bottom;
     } );
   }
+}
+
+/**
+ * Creates the label for an x-axis tick mark.
+ * @param {number} value
+ * @returns {Node}
+ */
+function createXTickLabel( value ) {
+  const coefficient = Utils.toFixedNumber( value / Math.PI, 0 );
+  const string = ( coefficient === 0 ) ? '0' : `${coefficient}${FMWSymbols.pi}`;
+  return new RichText( string, {
+    font: FMWConstants.TICK_LABEL_FONT,
+    maxWidth: 20
+  } );
 }
 
 fourierMakingWaves.register( 'ContinuousAmplitudesChartNode', ContinuousAmplitudesChartNode );
