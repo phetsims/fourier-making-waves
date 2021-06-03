49446764a0bb070a36cb7b3d98691032bb5d3166
phet-dev <phetadmin@gmail.com>
2021-06-03 03:33:29 -0600

    update copyright dates from daily grunt work

--- js/continuous/view/ContinuousSumEquationNode.js
+++ js/continuous/view/ContinuousSumEquationNode.js
@@ -1,4 +1,4 @@
-// Copyright 2020-2021, University of Colorado Boulder
+// Copyright 2021, University of Colorado Boulder
 
 /**
  * ContinuousSumEquationNode is the equation that appears above the 'Sum' chart in the 'Continuous' screen.
