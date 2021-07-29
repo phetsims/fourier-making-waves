154bf7d3c5c70b6087208467dfa0dcd12a4df1ab
pixelzoom <cmalley@pixelzoom.com>
2021-07-29 17:44:48 -0600

    reimplement AreaPlot

--- js/wavepacket/view/AreaUnderBellCurvePlot.js
+++ js/wavepacket/view/AreaUnderBellCurvePlot.js
@@ -6,11 +6,13 @@
  * @author Chris Malley (PixelZoom, Inc.)
  */
 
-import LinePlot from '../../../../bamboo/js/LinePlot.js';
+import Vector2 from '../../../../dot/js/Vector2.js';
+import Shape from '../../../../kite/js/Shape.js';
 import merge from '../../../../phet-core/js/merge.js';
+import Path from '../../../../scenery/js/nodes/Path.js';
 import fourierMakingWaves from '../../fourierMakingWaves.js';
 
-class AreaUnderBellCurvePlot extends LinePlot {
+class AreaUnderBellCurvePlot extends Path {
 
   /**
    * @param {ChartTransform} chartTransform
@@ -20,11 +22,39 @@ class AreaUnderBellCurvePlot extends LinePlot {
   constructor( chartTransform, dataSet, options ) {
 
     options = merge( {
-      stroke: null,
+
+      // Path options
       fill: 'black'
     }, options );
 
-    super( chartTransform, dataSet, options );
+    super( null, options );
+
+    // @private
+    this.chartTransform = chartTransform;
+
+    // @public if you change this directly, you are responsible for calling update
+    this.dataSet = dataSet;
+
+    // Initialize
+    this.update();
+
+    // Update when the transform changes.
+    const changedListener = () => this.update();
+    chartTransform.changedEmitter.addListener( changedListener );
+
+    // @private
+    this.disposeLinePlot = () => chartTransform.changedEmitter.removeListener( changedListener );
+  }
+
+  /**
+   * Sets the dataSet and redraws the plot. If instead the dataSet array is mutated, it is the client's responsibility
+   * to call `update` or make sure `update` is called elsewhere (say, if the chart scrolls in that frame).
+   * @param {Vector2[]} dataSet
+   * @public
+   */
+  setDataSet( dataSet ) {
+    this.dataSet = dataSet;
+    this.update();
   }
 
   /**
@@ -33,10 +63,55 @@ class AreaUnderBellCurvePlot extends LinePlot {
    * @override
    */
   update() {
-    super.update();
-    if ( this.dataSet.length > 1 ) {
-      this.shape.close();
+    assert && assert( _.every( this.dataSet, point => point !== null ), 'all points in data set must be non-null' );
+    assert && assert( _.every( this.dataSet, point => point.isFinite() ), 'all points in data set must be finite' );
+
+    const shape = new Shape();
+    const numberOfPoints = this.dataSet.length;
+    if ( numberOfPoints > 0 ) {
+      assert && assert( numberOfPoints > 1, 'at least 2 points are required to have an area under a curve' );
+
+      const firstModelPoint = this.dataSet[ 0 ];
+      const lastModelPoint = this.dataSet[ numberOfPoints - 1 ];
+      let startIndex = 0;
+      let viewPoint;
+
+      // Start at y=0
+      if ( firstModelPoint.y === 0 ) {
+        viewPoint = this.chartTransform.modelToViewPosition( firstModelPoint );
+        shape.moveToPoint( viewPoint );
+        startIndex++;
+      }
+      else {
+        viewPoint = this.chartTransform.modelToViewPosition( new Vector2( firstModelPoint.x, 0 ) );
+        shape.moveToPoint( viewPoint );
+      }
+
+      // Line segments to each point in the data set
+      for ( let i = startIndex; i < numberOfPoints; i++ ) {
+        viewPoint = this.chartTransform.modelToViewPosition( this.dataSet[ i ] );
+        shape.lineToPoint( viewPoint );
+      }
+
+      // End at y=0
+      if ( lastModelPoint.y !== 0 ) {
+        viewPoint = this.chartTransform.modelToViewPosition( new Vector2( lastModelPoint.x, 0 ) );
+        shape.lineToPoint( viewPoint );
+      }
+
+      // Close the Shape, so the Path can be filled.
+      shape.close();
     }
+    this.shape = shape;
+  }
+
+  /**
+   * @public
+   * @override
+   */
+  dispose() {
+    this.disposeLinePlot();
+    super.dispose();
   }
 }
 
