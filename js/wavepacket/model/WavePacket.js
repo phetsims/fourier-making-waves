5a732e31b94b050c53cc5e202e4eeed5093175f4
pixelzoom <cmalley@pixelzoom.com>
2021-08-05 15:28:54 -0600

    TODOs

--- js/wavepacket/model/WavePacket.js
+++ js/wavepacket/model/WavePacket.js
@@ -121,6 +121,9 @@ class WavePacket {
                              'In the time domain, this is in rad/ms.'
       } );
 
+    //TODO should this be called componentsProperty, because it identifies wave number (x) and amplitude (y)?
+    //TODO create a FourierComponent class that has wave number and amplitude fields, so it's not involved in Vector2 point caching?
+
     // @public {DerivedProperty.<Vector2[]>} the Fourier components used to approximate the wave packet
     // For each Vector2 instance, x is wave number, y is amplitude. Ordered by increasing wave number.
     // This is loosely based on the addGeneralPathPlot method in D2CAmplitudesView.java.
