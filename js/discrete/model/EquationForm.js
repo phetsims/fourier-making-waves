d2b0244431c5500feef008cebd02a96fe04bbe9e
phet-dev <phetadmin@gmail.com>
2021-08-11 03:36:44 -0600

    update copyright dates from daily grunt work

--- js/discrete/model/EquationForm.js
+++ js/discrete/model/EquationForm.js
@@ -1,4 +1,4 @@
-// Copyright 2020, University of Colorado Boulder
+// Copyright 2020-2021, University of Colorado Boulder
 
 /**
  * EquationForm enumerates the forms of the equations shown in the 'Discrete' screen.
