fd5f320b7e25d2f30a8a155a0b0fefafc03f20eb
pixelzoom <cmalley@pixelzoom.com>
2021-08-16 09:20:26 -0600

    revert to original lineWidths, https://github.com/phetsims/fourier-making-waves/issues/97

--- js/waveGame/view/WaveGameSumChartNode.js
+++ js/waveGame/view/WaveGameSumChartNode.js
@@ -28,7 +28,7 @@ class WaveGameSumChartNode extends SumChartNode {
 
       // SumChartNode options
       sumPlotStrokeProperty: FMWColors.answerSumPlotStrokeProperty,
-      sumPlotLineWidth: 3
+      sumPlotLineWidth: 4
     }, options );
 
     super( sumChart, options );
@@ -36,7 +36,7 @@ class WaveGameSumChartNode extends SumChartNode {
     // Plot that shows the sum for the answer
     const guessPlot = new CanvasLinePlot( this.chartTransform, [], {
       stroke: FMWColors.guessSumPlotStrokeProperty.value,
-      lineWidth: 2
+      lineWidth: 1.5
     } );
 
     // CanvasLinePlot does not allow stroke to be a Property, so we have to manage changes ourselves.
