890366848c8db07853d624b465eaedca147729f9
pixelzoom <cmalley@pixelzoom.com>
2021-09-20 16:50:56 -0600

    delete bogus visibleProperty option to CanvasLinePlot, https://github.com/phetsims/fourier-making-waves/issues/165

--- js/wavepacket/view/WavePacketAmplitudesChartNode.js
+++ js/wavepacket/view/WavePacketAmplitudesChartNode.js
@@ -103,10 +103,7 @@ class WavePacketAmplitudesChartNode extends DomainChartNode {
 
     // Displays the continuous waveform
     const continuousWaveformPlot = new CanvasLinePlot( this.chartTransform, [], {
-      lineWidth: FMWConstants.SECONDARY_WAVEFORM_LINE_WIDTH,
-      //REVIEW: Below notes say "CanvasLinePlot does not support visibleProperty", but I see a usage here
-      //REVIEW: Should this line be removed?
-      visibleProperty: continuousWaveformVisibleProperty
+      lineWidth: FMWConstants.SECONDARY_WAVEFORM_LINE_WIDTH
     } );
 
     // Render the plots using Canvas.
