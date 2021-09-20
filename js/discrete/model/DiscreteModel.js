307ee06d0cd3d5173f72fc63c699df37d1b4736d
pixelzoom <cmalley@pixelzoom.com>
2021-09-20 17:06:11 -0600

    delete options in instantiation of EmphasizedHarmonics, https://github.com/phetsims/fourier-making-waves/issues/165

--- js/discrete/model/DiscreteModel.js
+++ js/discrete/model/DiscreteModel.js
@@ -90,10 +90,7 @@ class DiscreteModel {
     } );
 
     // the harmonics to be emphasized in the Harmonics chart, as the result of UI interactions
-    const emphasizedHarmonics = new EmphasizedHarmonics( {
-      //REVIEW: EmphasizedHarmonics doesn't take any parameters
-      tandem: options.tandem.createTandem( 'emphasizedHarmonics' )
-    } );
+    const emphasizedHarmonics = new EmphasizedHarmonics();
 
     // Group elements related to measurement tools under this tandem.
     const measurementToolsTandem = options.tandem.createTandem( 'measurementTools' );
