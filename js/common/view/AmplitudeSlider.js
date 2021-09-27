8a60feac8b68b1e9cc0415a289fa014c6c6107c5
pixelzoom <cmalley@pixelzoom.com>
2021-09-27 12:59:43 -0600

    doc

--- js/common/view/AmplitudeSlider.js
+++ js/common/view/AmplitudeSlider.js
@@ -319,7 +319,9 @@ class BarTrack extends SliderTrack {
 //TODO https://github.com/phetsims/fourier-making-waves/issues/56 delete SoundDragHandler when Slider sound API is available
 /**
  * SoundDragHandler handles sound while dragging the thumb or track.
- * This must be used by both Slider and BarTrack in their options.drag.
+ * This must be used by both Slider and BarTrack in their options.drag, like this:
+ *
+ *   drag: event => soundDragHandler.drag( event )
  *
  * At the time of this implementation Slider had no sound API.
  * This is a temporary/adhoc implementation that is loosely based on WaveInterferenceSlider.js.
