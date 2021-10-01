eb0e6ca4b5631575285d260d49faaff42638a412
pixelzoom <cmalley@pixelzoom.com>
2021-10-01 17:16:25 -0600

    revise color for A3, https://github.com/phetsims/fourier-making-waves/issues/193

--- js/common/FMWColors.js
+++ js/common/FMWColors.js
@@ -104,7 +104,7 @@ const FMWColors = {
 const HARMONIC_COLORS = [
   new Color( 255, 0, 0 ),
   new Color( 255, 128, 0 ),
-  new Color( 255, 255, 0 ),
+  new Color( 255, 210, 0 ),
   new Color( 0, 255, 0 ),
   new Color( 0, 201, 87 ),
   new Color( 100, 149, 237 ),
