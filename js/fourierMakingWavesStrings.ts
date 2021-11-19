12832b6847c751216fa4d8dd9c2834c7c74686db
Sam Reid <reids@colorado.edu>
2021-11-18 17:43:32 -0700

    Run grunt update, see https://github.com/phetsims/chipper/issues/1148

---
+++ js/fourierMakingWavesStrings.ts
@@ -0,0 +1,129 @@
+// Copyright 2020-2021, University of Colorado Boulder
+
+/**
+ * Auto-generated from modulify, DO NOT manually modify.
+ */
+/* eslint-disable */
+import getStringModule from '../../chipper/js/getStringModule.js';
+import fourierMakingWaves from './fourierMakingWaves.js';
+
+type StringsType = {
+  'fourier-making-waves': {
+    'title': string
+  },
+  'screen': {
+    'discrete': string,
+    'waveGame': string,
+    'wavePacket': string
+  },
+  'fourierSeries': string,
+  'graphControls': string,
+  'measurementTools': string,
+  'sinusoid': string,
+  'triangle': string,
+  'square': string,
+  'sawtooth': string,
+  'wavePacket': string,
+  'custom': string,
+  'waveform': string,
+  'harmonics': string,
+  'harmonicsChart': string,
+  'functionOf': string,
+  'series': string,
+  'spaceSymbol': string,
+  'timeSymbol': string,
+  'spaceAndTimeSymbols': string,
+  'wavelength': string,
+  'period': string,
+  'equation': string,
+  'hidden': string,
+  'symbolAndSymbol': string,
+  'amplitude': string,
+  'sum': string,
+  'infiniteHarmonics': string,
+  'minToMax': string,
+  'enter': string,
+  'sawtoothWithCosines': string,
+  'expandedForm': string,
+  'symbol': {
+    'A': string,
+    'cos': string,
+    'd': string,
+    'F': string,
+    'L': string,
+    'T': string,
+    'f': string,
+    'k': string,
+    'lambda': string,
+    'n': string,
+    'omega': string,
+    'sigma': string,
+    'sin': string,
+    't': string,
+    'x': string
+  },
+  'symbolsDialog': {
+    'title': string,
+    'A': string,
+    'f': string,
+    'lambda': string,
+    'k': string,
+    'L': string,
+    'n': string,
+    't': string,
+    'T': string,
+    'x': string,
+    'sigma': string,
+    'omega': string
+  },
+  'levels': string,
+  'chooseYourLevel': string,
+  'matchUsing1Harmonic': string,
+  'matchUsingNHarmonics': string,
+  'matchUsingNOrMoreHarmonics': string,
+  'info1Harmonic': string,
+  'infoNHarmonics': string,
+  'infoNOrMoreHarmonics': string,
+  'checkAnswer': string,
+  'showAnswer': string,
+  'newWaveform': string,
+  'amplitudeControls': string,
+  'componentSpacing': string,
+  'wavePacketCenter': string,
+  'wavePacketWidth': string,
+  'amplitudesOfFourierComponents': string,
+  'fourierComponents': string,
+  'waveformEnvelope': string,
+  'widthIndicators': string,
+  'continuousWaveform': string,
+  'symbolValueUnits': string,
+  'symbolSymbolValueUnits': string,
+  'units': {
+    'meters': string,
+    'milliseconds': string,
+    'radiansPerMeter': string,
+    'radiansPerMillisecond': string
+  },
+  'symbolUnits': string,
+  'infiniteComponentsCannotBePlotted': string,
+  'keyboardHelpDialog': {
+    'gameControls': string,
+    'checkYourAnswer': string,
+    'measurementTools': string,
+    'moveTool': string,
+    'moveToolSlower': string
+  },
+  'a11y': {
+    'keyboardHelpDialog': {
+      'checkYourAnswerDescription': string,
+      'moveToolDescription': string,
+      'moveToolSlowerDescription': string
+    }
+  }
+};
+
+const fourierMakingWavesStrings = getStringModule( 'FOURIER_MAKING_WAVES' ) as StringsType;
+
+fourierMakingWaves.register( 'fourierMakingWavesStrings', fourierMakingWavesStrings );
+
+export default fourierMakingWavesStrings;
