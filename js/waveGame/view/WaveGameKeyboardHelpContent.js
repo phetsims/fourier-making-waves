0fae0973aae25d271da4de8bd4d764304e999719
phet-dev <phetadmin@gmail.com>
2022-01-05 03:19:43 -0700

    update copyright dates from daily grunt work

--- js/waveGame/view/WaveGameKeyboardHelpContent.js
+++ js/waveGame/view/WaveGameKeyboardHelpContent.js
@@ -1,4 +1,4 @@
-// Copyright 2021, University of Colorado Boulder
+// Copyright 2021-2022, University of Colorado Boulder
 
 /**
  * WaveGameKeyboardHelpContent is the content for the keyboard-help dialog in the 'Wave Game' screen.
