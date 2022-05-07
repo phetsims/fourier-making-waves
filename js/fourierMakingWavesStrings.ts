ecc87cd138a093fb52185038be3939c57eac7ff0
Sam Reid <reids@colorado.edu>
2022-05-07 09:34:07 -0600

    Use semicolons to terminate fields in type StringType, see https://github.com/phetsims/chipper/issues/1208

--- js/fourierMakingWavesStrings.ts
+++ js/fourierMakingWavesStrings.ts
@@ -9,109 +9,109 @@ import fourierMakingWaves from './fourierMakingWaves.js';
 
 type StringsType = {
   'fourier-making-waves': {
-    'title': string
-  },
+    'title': string;
+  };
   'screen': {
-    'discrete': string,
-    'waveGame': string,
-    'wavePacket': string
-  },
-  'fourierSeries': string,
-  'graphControls': string,
-  'measurementTools': string,
-  'sinusoid': string,
-  'triangle': string,
-  'square': string,
-  'sawtooth': string,
-  'wavePacket': string,
-  'custom': string,
-  'waveform': string,
-  'harmonics': string,
-  'harmonicsChart': string,
-  'functionOf': string,
-  'series': string,
-  'spaceSymbol': string,
-  'timeSymbol': string,
-  'spaceAndTimeSymbols': string,
-  'wavelength': string,
-  'period': string,
-  'equation': string,
-  'hidden': string,
-  'symbolAndSymbol': string,
-  'amplitude': string,
-  'sum': string,
-  'infiniteHarmonics': string,
-  'minToMax': string,
-  'enter': string,
-  'sawtoothWithCosines': string,
-  'expandedForm': string,
+    'discrete': string;
+    'waveGame': string;
+    'wavePacket': string;
+  };
+  'fourierSeries': string;
+  'graphControls': string;
+  'measurementTools': string;
+  'sinusoid': string;
+  'triangle': string;
+  'square': string;
+  'sawtooth': string;
+  'wavePacket': string;
+  'custom': string;
+  'waveform': string;
+  'harmonics': string;
+  'harmonicsChart': string;
+  'functionOf': string;
+  'series': string;
+  'spaceSymbol': string;
+  'timeSymbol': string;
+  'spaceAndTimeSymbols': string;
+  'wavelength': string;
+  'period': string;
+  'equation': string;
+  'hidden': string;
+  'symbolAndSymbol': string;
+  'amplitude': string;
+  'sum': string;
+  'infiniteHarmonics': string;
+  'minToMax': string;
+  'enter': string;
+  'sawtoothWithCosines': string;
+  'expandedForm': string;
   'symbol': {
-    'A': string,
-    'cos': string,
-    'd': string,
-    'F': string,
-    'L': string,
-    'T': string,
-    'f': string,
-    'k': string,
-    'lambda': string,
-    'n': string,
-    'omega': string,
-    'sigma': string,
-    'sin': string,
-    't': string,
-    'x': string
-  },
+    'A': string;
+    'cos': string;
+    'd': string;
+    'F': string;
+    'L': string;
+    'T': string;
+    'f': string;
+    'k': string;
+    'lambda': string;
+    'n': string;
+    'omega': string;
+    'sigma': string;
+    'sin': string;
+    't': string;
+    'x': string;
+  };
   'symbolsDialog': {
-    'title': string,
-    'A': string,
-    'f': string,
-    'lambda': string,
-    'k': string,
-    'L': string,
-    'n': string,
-    't': string,
-    'T': string,
-    'x': string,
-    'sigma': string,
-    'omega': string
-  },
-  'levels': string,
-  'chooseYourLevel': string,
-  'matchUsing1Harmonic': string,
-  'matchUsingNHarmonics': string,
-  'matchUsingNOrMoreHarmonics': string,
-  'info1Harmonic': string,
-  'infoNHarmonics': string,
-  'infoNOrMoreHarmonics': string,
-  'checkAnswer': string,
-  'showAnswer': string,
-  'newWaveform': string,
-  'amplitudeControls': string,
-  'componentSpacing': string,
-  'wavePacketCenter': string,
-  'wavePacketWidth': string,
-  'amplitudesOfFourierComponents': string,
-  'fourierComponents': string,
-  'waveformEnvelope': string,
-  'widthIndicators': string,
-  'continuousWaveform': string,
-  'symbolValueUnits': string,
-  'symbolSymbolValueUnits': string,
+    'title': string;
+    'A': string;
+    'f': string;
+    'lambda': string;
+    'k': string;
+    'L': string;
+    'n': string;
+    't': string;
+    'T': string;
+    'x': string;
+    'sigma': string;
+    'omega': string;
+  };
+  'levels': string;
+  'chooseYourLevel': string;
+  'matchUsing1Harmonic': string;
+  'matchUsingNHarmonics': string;
+  'matchUsingNOrMoreHarmonics': string;
+  'info1Harmonic': string;
+  'infoNHarmonics': string;
+  'infoNOrMoreHarmonics': string;
+  'checkAnswer': string;
+  'showAnswer': string;
+  'newWaveform': string;
+  'amplitudeControls': string;
+  'componentSpacing': string;
+  'wavePacketCenter': string;
+  'wavePacketWidth': string;
+  'amplitudesOfFourierComponents': string;
+  'fourierComponents': string;
+  'waveformEnvelope': string;
+  'widthIndicators': string;
+  'continuousWaveform': string;
+  'symbolValueUnits': string;
+  'symbolSymbolValueUnits': string;
   'units': {
-    'meters': string,
-    'milliseconds': string,
-    'radiansPerMeter': string,
-    'radiansPerMillisecond': string
-  },
-  'symbolUnits': string,
-  'infiniteComponentsCannotBePlotted': string,
+    'meters': string;
+    'milliseconds': string;
+    'radiansPerMeter': string;
+    'radiansPerMillisecond': string;
+  };
+  'symbolUnits': string;
+  'infiniteComponentsCannotBePlotted': string;
   'keyboardHelpDialog': {
-    'gameControls': string,
-    'checkYourAnswer': string,
-    'measurementTools': string,
-    'moveTool': string,
-    'moveToolSlower': string
+    'gameControls': string;
+    'checkYourAnswer': string;
+    'measurementTools': string;
+    'moveTool': string;
+    'moveToolSlower': string;
   }
 };
 
