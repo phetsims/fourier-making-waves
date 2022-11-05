9fddb7b79c30d8e3cb3786eacf0473ce9bebbe4b
phet-dev <phetadmin@gmail.com>
2022-11-05 04:08:24 -0600

    update copyright dates from daily grunt work

--- js/wavepacket/view/LengthToolCheckbox.js
+++ js/wavepacket/view/LengthToolCheckbox.js
@@ -1,4 +1,4 @@
-// Copyright 2021, University of Colorado Boulder
+// Copyright 2021-2022, University of Colorado Boulder
 
 /**
  * LengthToolCheckbox is the checkbox for changing visibility of the Component Spacing tool in the
