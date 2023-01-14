ba32d69ad007b1e5601e832fe6b96da4c8e37c13
phet-dev <phetadmin@gmail.com>
2023-01-14 04:23:36 -0700

    update copyright dates from daily grunt work

--- js/common/view/SeriesTypeRadioButtonGroup.js
+++ js/common/view/SeriesTypeRadioButtonGroup.js
@@ -1,4 +1,4 @@
-// Copyright 2020-2022, University of Colorado Boulder
+// Copyright 2020-2023, University of Colorado Boulder
 
 /**
  * SeriesTypeRadioButtonGroup is used to switch between sine series and cosine series.
