a11650c069e842d38db6f9e596fcb665a7ff2ef3
pixelzoom <cmalley@pixelzoom.com>
2023-04-25 16:02:37 -0600

    rename .js to .ts

--- js/wavepacket/WavePacketScreen.ts
+++ js/wavepacket/WavePacketScreen.ts
@@ -7,6 +7,7 @@
  */
 
 import Screen from '../../../joist/js/Screen.js';
+import Tandem from '../../../tandem/js/Tandem.js';
 import FMWColors from '../common/FMWColors.js';
 import FMWIconFactory from '../common/view/FMWIconFactory.js';
 import fourierMakingWaves from '../fourierMakingWaves.js';
@@ -15,21 +16,15 @@ import WavePacketModel from './model/WavePacketModel.js';
 import WavePacketKeyboardHelpContent from './view/WavePacketKeyboardHelpContent.js';
 import WavePacketScreenView from './view/WavePacketScreenView.js';
 
-export default class WavePacketScreen extends Screen {
+export default class WavePacketScreen extends Screen<WavePacketModel, WavePacketScreenView> {
 
-  constructor( tandem ) {
+  public constructor( tandem: Tandem ) {
 
     const options = {
-
-      // Screen options
       name: FourierMakingWavesStrings.screen.wavePacketStringProperty,
       backgroundColorProperty: FMWColors.wavePacketScreenBackgroundColorProperty,
       homeScreenIcon: FMWIconFactory.createWavePacketHomeScreenIcon(),
-
-      // pdom options
       createKeyboardHelpNode: () => new WavePacketKeyboardHelpContent(),
-
-      // phet-io options
       tandem: tandem
     };
 
@@ -40,11 +35,7 @@ export default class WavePacketScreen extends Screen {
     );
   }
 
-  /**
-   * @public
-   * @override
-   */
-  dispose() {
+  public override dispose(): void {
     assert && assert( false, 'dispose is not supported, exists for the lifetime of the sim' );
     super.dispose();
   }
