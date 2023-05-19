4d80d3899f0fbc7b73e37c3bca462d631f5dd13f
phet-dev <phetadmin@gmail.com>
2023-05-19 03:39:51 -0600

    update copyright dates from daily grunt work

--- js/common/view/FMWIconFactory.ts
+++ js/common/view/FMWIconFactory.ts
@@ -1,4 +1,4 @@
-// Copyright 2021-2022, University of Colorado Boulder
+// Copyright 2021-2023, University of Colorado Boulder
 
 /**
  * FMWIconFactory contains factory methods for creating the icons needed throughout this sim's user interface.
