4bf99093b28a140d3c1827aa3aeb8560f07beee2
pixelzoom <cmalley@pixelzoom.com>
2023-05-18 19:32:11 -0600

    convert to TypeScript

--- js/wavepacket/view/WavePacketScreenView.ts
+++ js/wavepacket/view/WavePacketScreenView.ts
@@ -34,13 +34,7 @@ import WavePacketSumEquationNode from './WavePacketSumEquationNode.js';
 
 export default class WavePacketScreenView extends ScreenView {
 
-  /**
-   * @param {WavePacketModel} model
-   * @param {Tandem} tandem
-   */
-  constructor( model, tandem ) {
-    assert && assert( model instanceof WavePacketModel );
-    assert && assert( tandem instanceof Tandem );
+  public constructor( model: WavePacketModel, tandem: Tandem ) {
 
     super( {
       tandem: tandem
@@ -378,11 +372,7 @@ export default class WavePacketScreenView extends ScreenView {
     ];
   }
 
-  /**
-   * @public
-   * @override
-   */
-  dispose() {
+  public override dispose(): void {
     assert && assert( false, 'dispose is not supported, exists for the lifetime of the sim' );
     super.dispose();
   }
