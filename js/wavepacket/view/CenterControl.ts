accc9d5fd3b79053af7dc311c3718f93a382ba6e
phet-dev <phetadmin@gmail.com>
2024-01-09 03:13:05 -0700

    update copyright dates from daily grunt work

--- js/wavepacket/view/CenterControl.ts
+++ js/wavepacket/view/CenterControl.ts
@@ -1,4 +1,4 @@
-// Copyright 2021-2023, University of Colorado Boulder
+// Copyright 2021-2024, University of Colorado Boulder
 
 /**
  * CenterControl controls the wave packet's center (k0 or omega0).
