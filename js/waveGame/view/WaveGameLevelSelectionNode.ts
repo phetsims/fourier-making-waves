a37ad79b4f77d8a182a58a6decfc96f229de2dc1
pixelzoom <cmalley@pixelzoom.com>
2024-07-01 11:21:55 -0600

    interrupt interactions when resetAllButton is pressed

--- js/waveGame/view/WaveGameLevelSelectionNode.ts
+++ js/waveGame/view/WaveGameLevelSelectionNode.ts
@@ -60,7 +60,10 @@ export default class WaveGameLevelSelectionNode extends Node {
 
     // Reset All button, at lower right
     const resetAllButton = new ResetAllButton( {
-      listener: () => model.reset(),
+      listener: () => {
+        this.interruptSubtreeInput();
+        model.reset();
+      },
       right: layoutBounds.maxX - FMWConstants.SCREEN_VIEW_X_MARGIN,
       bottom: layoutBounds.maxY - FMWConstants.SCREEN_VIEW_Y_MARGIN,
       tandem: tandem.createTandem( 'resetAllButton' )
