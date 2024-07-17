ad66cca08512d53daf321bc1eec825c943526cc9
phet-dev <phetadmin@gmail.com>
2024-07-17 03:45:25 -0600

    update copyright dates from daily grunt work

--- js/discrete/view/DiscreteHarmonicsChartNode.ts
+++ js/discrete/view/DiscreteHarmonicsChartNode.ts
@@ -1,4 +1,4 @@
-// Copyright 2021-2023, University of Colorado Boulder
+// Copyright 2021-2024, University of Colorado Boulder
 
 /**
  * DiscreteHarmonicsChartNode displays the 'Harmonics' chart in the 'Discrete' screen. It extends HarmonicsChartNode
