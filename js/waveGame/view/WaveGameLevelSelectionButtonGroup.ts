a74aa0ae40ea90c8ec45cb292ab9d39d9d529620
phet-dev <phetadmin@gmail.com>
2025-10-11 04:13:44 -0600

    update copyright dates from daily grunt work

--- js/waveGame/view/WaveGameLevelSelectionButtonGroup.ts
+++ js/waveGame/view/WaveGameLevelSelectionButtonGroup.ts
@@ -1,4 +1,4 @@
-// Copyright 2022-2024, University of Colorado Boulder
+// Copyright 2022-2025, University of Colorado Boulder
 
 /**
  * WaveGameLevelSelectionButtonGroup is the group of level-selection buttons for the 'Wave Game' screen.
